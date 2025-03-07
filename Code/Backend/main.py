d3a73336ed727ed90506c5db5e43da0fc494879b
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2025-03-07 16:51:37 +0800

    add instructions to set up iris intersystems

--- Code/Backend/main.py
+++ Code/Backend/main.py
@@ -1,5 +1,6 @@
 import os
 import json
+from DBhelpers.py import *
 from google import genai
 from dotenv import load_dotenv
 
@@ -14,29 +15,22 @@ port = '1972'
 namespace = 'USER'
 
 
+# with open("PROM Corpus/EQ-5D-5L_corpus.json") as file:
+#     data = json.load(file)
 
-with open("PROM Corpus/EQ-5D-5L_corpus.json") as file:
-    data = json.load(file)
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GENAI_API_KEY"))
 
 chat = client.chats.create(model="gemini-2.0-flash")
-# response = client.models.generate_content(
-#     model="gemini-2.0-flash",
-#     contents="Explain how AI works",
-# )
+connect_to_IRIS(username, password, hostname, port, namespace)
 
+# initial_prompt = f"You are a chatbot collecting Patient Reported Outcomes (PROs) EQ-5D-5L_corpus. Please ask the patient the following questions: {data[0]["question"]}"
+
+# response = chat.send_message(initial_prompt)
 # print(response.text)
-initial_prompt = f"You are a chatbot collecting Patient Reported Outcomes (PROs) EQ-5D-5L_corpus. Please ask the patient the following questions: {data[0]["question"]}"
-# prompt = input("You: ")
-response = chat.send_message(initial_prompt)
-print(response.text)
-while True:
-    new_prompt = input("You: ")
-    if new_prompt == "exit":
-        break
-    response = chat.send_message(new_prompt)
-    print(response.text)
-    # for message in chat._curated_history:
-    #     print(f"role - {message.role}", end=": ")
-    #     print(message.parts[0].text)
+# while True:
+#     new_prompt = input("You: ")
+#     if new_prompt == "exit":
+#         break
+#     response = chat.send_message(new_prompt)
+#     print(response.text)
