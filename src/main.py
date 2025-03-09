396b0d1cfabfda7018617ac361cff06d0dee06cb
YZTangent <yuanzheng.tan.tyz@gmail.com>
2025-03-09 15:49:46 +0900

    Add LLM connection

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from database import dbcontext
+from model.model import Chatbot
 
 if __name__ == "__main__":
     username = 'demo'
@@ -8,6 +9,7 @@ if __name__ == "__main__":
     namespace = 'USER'
 
     db = dbcontext.DbContext(username, password, hostname, port, namespace)
-    print(db.get_all_questions())
-    print(db.get_question_options(1))
-    print(db.search_question_option(1, "i am gay", 10))
+    bot = Chatbot(db)
+
+    r = bot.test()
+    print(r)
--- src/model/model.py
+++ src/model/model.py
@@ -1,66 +1,121 @@
-import vertexai
-from tools import control_flow_tools
-
-from vertexai.generative_models import (
-    Content,
-    FunctionDeclaration,
-    GenerationConfig,
-    GenerativeModel,
-    Part,
-    Tool,
-)
-
-# TODO(developer): Update & uncomment below line
-PROJECT_ID = "health-hack-2025"
-LOCATION = "us-west1"
-
-# Initialize Vertex AI
-vertexai.init(project=PROJECT_ID, location=LOCATION)
-
-# Initialize Gemini model
-model = GenerativeModel("gemini-2.0-flash-001")
-
-# Define the user's prompt in a Content object that we can reuse in model calls
-user_prompt_content = Content(
-    role="user",
-    parts=[
-        Part.from_text("I find it hard to move around."),
-    ],
-)
+from google import genai
+from google.genai import types
+from model.tools import control_flow_tools
 
 
-# Send the prompt and instruct the model to generate content using the Tool that you just created
-response = model.generate_content(
-    user_prompt_content,
-    generation_config=GenerationConfig(temperature=0),
-    tools=[control_flow_tools],
-)
-function_call = response.candidates[0].function_calls[0]
-# print(function_call)
-# print(response.text)
-print(response.candidates)
+class Chatbot:
+    client = None
+    dbcontext = None
+    model = "gemini-2.0-flash-001"
+    generate_content_config = types.GenerateContentConfig(
+        temperature=0.2,
+        # top_p=0.5,
+        # max_output_tokens=8192,
+        # safety_settings=[types.SafetySetting(
+        #     category="HARM_CATEGORY_HATE_SPEECH",
+        #     threshold="OFF"
+        # ), types.SafetySetting(
+        #     category="HARM_CATEGORY_DANGEROUS_CONTENT",
+        #     threshold="OFF"
+        # ), types.SafetySetting(
+        #     category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        #     threshold="OFF"
+        # ), types.SafetySetting(
+        #     category="HARM_CATEGORY_HARASSMENT",
+        #     threshold="OFF"
+        # )],
+        # Function signatures for tool calling.
+        # Comment out the tools config below to disable function calling.
+        # tools=[
+        #     control_flow_tools
+        # ]
+    )
 
+    def __init__(self, dbcontext):
+        if Chatbot.client is None:
+            Chatbot.client = genai.Client(
+                vertexai=True,
+                project="health-hack-2025",
+                location="us-west1",
 
-# # Return the API response to Gemini so it can generate a model response or request another function call
-# response = model.generate_content(
-#     [
-#         user_prompt_content,  # User prompt
-#         response.candidates[0].content,  # Function call response
-#         Content(
-#             parts=[
-#                 Part.from_function_response(
-#                     name=function_name,
-#                     response={
-#                         "content": api_response,  # Return the API response to Gemini
-#                     },
-#                 ),
-#             ],
-#         ),
-#     ],
-#     tools=[weather_tool],
-# )
+                # API key authentication is not supported by vertex ai.
+                # Instead, authenticate using Google ADC.
+                # api_key=gemini_apik_key
+            )
+            Chatbot.dbcontext = dbcontext
 
-if __name__ == "__main__":
-    conversation_ongoing = True
+    def create_session(self):
+        pass
 
-    while conversation_ongoing
+    def test(self):
+        response = Chatbot.client.models.generate_content(
+            model=Chatbot.model,
+            contents="""You are clinical doctor collecting electronic Patient Reported Outcomes (ePROs) 
+                to improve healthcare quality and enhance patients' quality of life post-procedure. 
+                Interview question style must be conversational, empathetic, and encourage higher levels of self disclosure.
+                Ask follow up questions until you are able to get a satisfactory answer from the patient. 
+                """
+            +
+            """Please ask the patient about his mobility.
+                Now, start this conversation with the patient""",
+            config=Chatbot.generate_content_config
+        )
+        return response.text
+#
+#     def give_instruction():
+#         chat.send_message(
+#             """You are clinical doctor collecting electronic Patient Reported Outcomes (ePROs)
+#                 to improve healthcare quality and enhance patients' quality of life post-procedure.
+#                 Interview question style must be conversational, empathetic, and encourage higher levels of self disclosure.
+#                 Ask follow up questions until you are able to get a satisfactory answer from the patient.
+#                 """
+#         )
+#
+#     def generate_prompt(qn_num):
+#         question_object = Chatbot.dbcontext.get_question_options(qn_num)
+#         return f"""Please ask the patient about {question_object['question']}.
+#                 You can refer to these possible answers {question_object['options']}.
+#                 Now, start this conversation with the patient"""
+#
+#
+# # parameters: msg: string
+# # return: response: string
+# def send_msg_to_genai(msg):
+#     response = chat.send_message(msg)
+#     return response.text
+# # parameters: msg: string
+# # return: response: None
+#
+#
+# def display_msg_to_user(msg):
+#     print(msg)
+#
+# # parameters: None
+# # return: response: string
+#
+#
+# def get_response_from_user():
+#     response = input("User: ")
+#
+#     # implement save_response_to_database(response)
+#
+#     return response
+#
+# # Start Chat
+#
+#
+# def start_chat(chat):
+#     give_instruction()
+#     for qns in range(0, 5):
+#         response = chat.send_message(generate_prompt(qns))
+#         display_msg_to_user(response.text)
+#         for rounds in range(0, 3):
+#             user_response = get_response_from_user()
+#             # "exit" is a hard stop to terminate the program immediately
+#             if user_response == "exit":
+#                 return
+#             response = send_msg_to_genai(user_response)
+#             display_msg_to_user(response)
+#
+#
+# start_chat(chat)
